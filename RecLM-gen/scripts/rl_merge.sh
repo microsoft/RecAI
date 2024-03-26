d62d78d048da0f4c4d4a576909b2fa88cc35368e
YuxuanLei <47939326+Micheallei@users.noreply.github.com>
2024-03-26 11:26:30 +0800

    Address information leakage issue; delete Chinese annotations (#22)
    Co-authored-by: v-leiyuxuan <v-leiyuxuan@microsoft.com>

--- RecLM-gen/scripts/rl_merge.sh
+++ RecLM-gen/scripts/rl_merge.sh
@@ -11,7 +11,7 @@ CUDA_VISIBLE_DEVICES=8 python main.py \
   --RL_actor_lora_a 2 \
   --RL_critic_lora_r 4 \
   --RL_critic_lora_a 2 \
-  --RL_load /home/lws/projects/RecLM-gen/snap/ICR_SubMovie_Title64T_0_Llama7bChat_LCT_E40_CCR2_SCG2-0.5_IDX/RL_Total_train_LM-True_VM-False_NR-20.1_SN-2_Q-False_T6_FG-True_LR-5e-06_LDO-0.0_WD-0.0_KLC-0.3_EW-0.01_RS-False_RW-True_VFC-0.1_KLT-0.05_LRP-2.0_GAMMA-0.99_GAS-4_LB-1_RA_0.5_/4800step_RL \
+  --RL_load snap/ICR_SubMovie_Title64T_0_Llama7bChat_LCT_E40_CCR2_SCG2-0.5_IDX/RL_Total_train_LM-True_VM-False_NR-20.1_SN-2_Q-False_T6_FG-True_LR-5e-06_LDO-0.0_WD-0.0_KLC-0.3_EW-0.01_RS-False_RW-True_VFC-0.1_KLT-0.05_LRP-2.0_GAMMA-0.99_GAS-4_LB-1_RA_0.5_/4800step_RL \
   --lm_head_full_tune \
   --FA2
 
